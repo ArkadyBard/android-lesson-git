618b91b2e9122e6f5956e5a5393bb29ac2551bf4
Arkady <asdarka1994@gmail.com>
2020-12-14 20:50:13 +0800

    added three variables in MAinActivity

--- app/src/main/java/com/arkadybardamov/myapplication/MainActivity.kt
+++ app/src/main/java/com/arkadybardamov/myapplication/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val a = 0
+        val b = 0
+        val c = 0
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
