19adaf86c7b03b97614597db721a0341a3395505
Arkady <asdarka1994@gmail.com>
2020-12-18 22:45:37 +0800

    Added OnClick for button

--- app/src/main/java/com/arkadybardamov/myapplication/MainActivity.kt
+++ app/src/main/java/com/arkadybardamov/myapplication/MainActivity.kt
@@ -2,15 +2,29 @@ package com.arkadybardamov.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+/**
+ * Основная активность
+ * добавил обработчик кнопки
+ *
+ * */
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
-        val a = 0
-        val b = 0
-        val c = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button_1 = findViewById<Button>(R.id.btn_1)
+        val textView_1 = findViewById<TextView>(R.id.tv_1)
+
+        button_1.setOnClickListener {
+            textView_1.text = getString(R.string.hello_string)
+        }
+
     }
 }
